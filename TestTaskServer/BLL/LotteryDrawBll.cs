43999276cc876069087d28d9b2035cffb677ccb7
XQ <546247083@qq.com>
2017-04-10 13:51:34 +0800

    缓存用户抽奖信息

---
+++ TestTaskServer/BLL/LotteryDrawBll.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace TestTaskServer
+{
+    public class LotteryDrawBll
+    {
+        /// <summary>
+        /// 获取排行榜
+        /// </summary>
+        /// <param name="userFlag">用户标志</param>
+        /// <returns>排行榜数据</returns>
+        public DataTable GetCharts(ref string userFlag)
+        {
+            //获取当前用户分数
+            userFlag = LotteryDrawDal.Instance.GetUserPoints(userFlag).ToString();
+            //刷新排行榜
+            LotteryDrawDal.Instance.RefreshCharts(0);
+            return LotteryDrawDal.Instance.LotteryDrawChartsDT;
+        }
+    }
+}
